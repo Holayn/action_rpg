0a32cc5c8945a710c347e6fcd8b217fbf7d4ea81
Holayn <kwwong1597@gmail.com>
2020-07-20 23:02:25 -0400

    remove old animation code logic in person

--- Player/person.gd
+++ Player/person.gd
@@ -10,14 +10,6 @@ var speed = 4
 var move_dir = Vector2.ZERO
 
 var direction = Vector2.ZERO
-#var sprite_dir = "right"
-
-#var direction_map = {
-#	Vector2(-1,0): "left",
-#	Vector2(1,0): "right",
-#	Vector2(0,1): "down",
-#	Vector2(0,-1): "up",
-#}
 
 func _ready():
 	if $animTree:
@@ -29,15 +21,12 @@ func set_move_anim_tree():
 func set_idle_anim_tree():
 	pass
 
-
 func movement_state(delta):
 	if move_dir != Vector2(0,0):
 		set_move_anim_tree()
 		$animTree.get("parameters/playback").travel("run")
 		velocity = velocity.move_toward(move_dir * SPEED, ACCEL * delta)
-#		animation_player("run")
 	else:
-#		animation_player("idle")
 		set_idle_anim_tree()
 		$animTree.get("parameters/playback").travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -46,11 +35,3 @@ func movement_state(delta):
 
 func attack_animation_finished():
 	state = Enums.MOVE
-
-#func sprite_dir_process():
-#	var direction = direction_map.get(move_dir)
-#	if direction != null:
-#		sprite_dir = direction
-#
-#func animation_player(action):
-#	$anim.play(str(action,sprite_dir))
